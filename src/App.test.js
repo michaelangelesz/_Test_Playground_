92fe5fc5f10849ff31dfdd3430100495f52c2d4d
michaelangelesz <125311967+michaelangelesz@users.noreply.github.com>
2023-08-16 20:38:52 -0400

    create test playground

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,35 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import userEvent from '@testing-library/user-event';
 
-test('renders learn react link', () => {
+test('header renders with correct text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const headerE1 = screen.getByRole('heading')
+expect(headerE1.textContent).toBe('Test Playground')
+  // const linkElement = screen.getByText(/learn react/i);
+  // expect(linkElement).toBeInTheDocument();
 });
+
+describe('test for the button', () =>{
+  test("button changes color when clicked", () => {
+    render(<App />)
+    const colorBtn = screen.getByRole("button")
+  
+    userEvent.click(colorBtn)
+  
+    expect(colorBtn).toHaveStyle({ backgroundColor: "red" })
+    expect(colorBtn.textContent).toBe("Change to blue")
+  })
+}) 
+
+test("checkbox disables button when first clicked, then enables button on second click", () => {
+  render(<App />)
+  const colorBtn = screen.getByRole("button")
+  const checkbox = screen.getByRole("checkbox")
+
+  userEvent.click(checkbox)
+  expect(colorBtn).toBeDisabled()
+
+  userEvent.click(checkbox)
+  expect(colorBtn).toBeEnabled()
+})
